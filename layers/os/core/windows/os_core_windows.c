98e9164d7f0acd717b18613a5e8742593a07e59e
Beau McCartney <mccartney.beausl@gmail.com>
2024-12-09 16:13:45 -0700

    windows backend for everything i have so far

---
+++ layers/os/core/windows/os_core_windows.c
@@ -0,0 +1,47 @@
+#pragma push_macro("function")
+#undef function
+#include <windows.h>
+#pragma pop_macro("function")
+
+function OS_SystemInfo os_get_system_info(void)
+{
+	SYSTEM_INFO info = {0};
+	GetSystemInfo(&info);
+
+	OS_SystemInfo result = {
+		.page_size = info.dwPageSize,
+		.large_page_size = GetLargePageMinimum(),
+	};
+
+	return result;
+}
+
+function void *os_reserve(U64 size)
+{
+	return VirtualAlloc(0, size, MEM_RESERVE, PAGE_READWRITE);
+}
+
+function B32 os_commit(void *ptr, U64 size)
+{
+	return VirtualAlloc(ptr, size, MEM_COMMIT, PAGE_READWRITE) != 0;
+}
+
+function void os_decommit(void *ptr, U64 size)
+{
+	VirtualFree(ptr, size, MEM_DECOMMIT);
+}
+
+function void os_release(void *ptr, U64 size)
+{
+	VirtualFree(ptr, size, MEM_RELEASE);
+}
+
+function void *os_reserve_large(U64 size)
+{
+	return VirtualAlloc(0, size, MEM_RESERVE|MEM_COMMIT|MEM_LARGE_PAGES, PAGE_READWRITE);
+}
+
+function B32 os_commit_large(void *ptr, U64 size)
+{
+	return 1;
+}
--- layers/os/os_inc.c
+++ layers/os/os_inc.c
@@ -1,5 +1,5 @@
 #if OS_MAC
-
-#include "core/mac/os_core_mac.c"
-
+	#include "core/mac/os_core_mac.c"
+#elif OS_WINDOWS
+	#include "core/windows/os_core_windows.c"
 #endif // OS_MAC
