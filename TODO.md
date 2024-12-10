bee4bccd8399031faefde9b5d374b66ece91db1e
Beau McCartney <mccartney.beausl@gmail.com>
2024-12-09 17:22:05 -0700

    improved build file

--- TODO.md
+++ TODO.md
@@ -1,14 +1,14 @@
 - [x] context cracking
 - [x] arena
     - [x] vm grow strategy
-    - [ ] from given byte slice
 - [ ] math lib
 - [ ] ring
     - [ ] vm backend
 - [ ] thread context
 - [ ] build file improvements
-    - [ ] options by passing arguments (for each argument set a var with that
+    - [x] options by passing arguments (for each argument set a var with that
           args name then later check the var to toggle its associated
           functionality)
     - [ ] build.sh for mac
     - [ ] good cl flags
+- [ ] ctags setup
--- build.bat
+++ build.bat
@@ -1,9 +1,27 @@
 @echo off
 setlocal enabledelayedexpansion
+cd /D "%~dp0"
+
+for %%a in (%*) do set "%%a=1"
+if not "%release%"=="1" set debug=1
+if "%debug%"=="1"   set release=0 && echo [debug mode]
+if "%release%"=="1" set debug=0 && echo [release mode]
+
+set auto_compile_flags=
+if "%asan%"=="1" set auto_compile_flags=%auto_compile_flags% -fsanitize=address && echo [asan enabled]
+
+set cl_common= /I..\layers\ /I..\scratch\ /nologo /Z7
+set cl_debug= /Od /Ob1 /DBUILD_DEBUG=1 %cl_common%
+
+set build_kind_flags=
+if "%debug%"=="1" set build_kind_flags=%cl_debug%
+
+if not exist build mkdir build
+if not exist scratch mkdir scratch
 
 pushd build
 
-cl /nologo /Z7 /I..\layers ..\scratch\main.c
-main
+del *.pdb
+cl %cl_common% %build_kind_flags% %auto_compile_flags% ..\scratch\main.c /link /MANIFEST:EMBED /INCREMENTAL:NO /pdbaltpath:%%%%_PDB%%%%
 
 popd
