2a876167c8c12dcf3291b2cdb3fd695ebcf37983
Beau McCartney <mccartney.beausl@gmail.com>
2024-12-10 01:25:06 -0700

    single flags accumulator and toggle for RTC
    note that /RTCu is only turned on with no asan cause it trips when asan does its thing

--- build.bat
+++ build.bat
@@ -1,6 +1,3 @@
-:: TODO(beau):
-:: single flags accumulator
-
 @echo off
 setlocal enabledelayedexpansion
 cd /D "%~dp0"
@@ -10,14 +7,12 @@ if not "%release%"=="1" set debug=1
 if "%debug%"=="1"   set release=0 && echo [debug mode]
 if "%release%"=="1" set debug=0 && echo [release mode]
 
-set auto_compile_flags=
-if "%asan%"=="1" set auto_compile_flags=%auto_compile_flags% -fsanitize=address && (echo [asan enabled]) else set auto_compile_flags=%auto_compile_flags% /RTCc
+:: common cl flags
+set flags=/I..\layers\ /I..\scratch\ /nologo /Z7 /FC /W4 /wd4100
 
-set cl_common= /I..\layers\ /I..\scratch\ /nologo /Z7 /FC /W4 /wd4100 /RTCsu
-set cl_debug= /Od /Ob1 /DBUILD_DEBUG=1
-
-set build_kind_flags=
-if "%debug%"=="1" set build_kind_flags=%cl_debug%
+if "%debug%"=="1" set flags=%flags% /Od /Ob1 /DBUILD_DEBUG=1
+if "%asan%"=="1"  set flags=%flags% -fsanitize=address && (echo [asan enabled])
+if "%RTC%"=="1"   (if "%asan%"=="1" set flags=%flags% /RTCsu && (echo [RTC enabled]) else (set flags=%flags% /RTCcsu)) :: RTCu will trip on asan itself if its on, so only turn it on if no asan
 
 if not exist build mkdir build
 if not exist scratch mkdir scratch
@@ -27,6 +22,6 @@ pushd build
 del *.pdb
 
 @echo on
-cl %cl_common% %build_kind_flags% %auto_compile_flags% ..\apps\test\main.c /link /MANIFEST:EMBED /INCREMENTAL:NO /pdbaltpath:%%%%_PDB%%%%
+cl %flags% ..\apps\test\main.c /link /MANIFEST:EMBED /INCREMENTAL:NO /pdbaltpath:%%%%_PDB%%%%
 
 popd
