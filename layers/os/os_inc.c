e25db280971750ae03dbb0e5007960e946323670
Beau McCartney <mccartney.beausl@gmail.com>
2024-12-23 23:37:02 -0700

    #define for if the app will be graphical

--- layers/os/os_inc.c
+++ layers/os/os_inc.c
@@ -1,3 +1,8 @@
+#include "core/os_core.c"
+#if OS_FEATURE_GRAPHICAL
+	#include "gfx/os_gfx.c"
+#endif
+
 #if OS_MAC
 	#include "core/mac/os_core_mac.c"
 #elif OS_WINDOWS
--- layers/os/os_inc.h
+++ layers/os/os_inc.h
@@ -1,7 +1,13 @@
 #ifndef OS_INC_H
 #define OS_INC_H
 
+#ifndef OS_FEATURE_GRAPHICAL
+	#define OS_FEATURE_GRAPHICAL 0
+#endif
+
 #include "core/os_core.h"
-#include "gfx/os_gfx.h"
+#if OS_FEATURE_GRAPHICAL
+	#include "gfx/os_gfx.h"
+#endif
 
 #endif // OS_INC_H
