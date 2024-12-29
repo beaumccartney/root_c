36be8eaae234471de440ba42a8252b4ee36bdacf
Beau McCartney <mccartney.beausl@gmail.com>
2024-12-28 23:28:19 -0700

    base profile layer ontop of superluminal

--- layers/base/base_inc.h
+++ layers/base/base_inc.h
@@ -4,6 +4,7 @@
 #include "base_context_cracking.h"
 
 #include "base_core.h"
+#include "base_profile.h"
 #include "base_arena.h"
 #include "base_math.h"
 #include "base_strings.h"
---
+++ layers/base/base_profile.h
@@ -0,0 +1,30 @@
+#ifndef BASE_PROFILE_H
+#define BASE_PROFILE_H
+
+#ifndef PROFILE_SUPERLUMINAL
+	#define PROFILE_SUPERLUMINAL 0
+#endif
+
+#ifndef PROFILE_TRACY
+	#define PROFILE_TRACY 0
+#endif
+
+#if PROFILE_SUPERLUMINAL
+	#if !OS_WINDOWS
+		#error Superluminal profiling only supported on windows
+	#endif
+	#include "Superluminal/PerformanceAPI_capi.h"
+	#pragma comment(lib, "PerformanceAPI_MT.lib")
+
+	#define ProfBegin(name_cstr_lit)  PerformanceAPI_BeginEvent((name_cstr_lit), 0, PERFORMANCEAPI_DEFAULT_COLOR)
+	#define ProfEnd()                 PerformanceAPI_EndEvent()
+	#define ProfThreadName(name_cstr) PerformanceAPI_SetCurrentThreadName((name_cstr))
+#endif // PROFILE_SUPERLUMINAL
+
+#ifndef ProfBegin
+	#define ProfBegin(name_cstr_lit)  (0)
+	#define ProfEnd()                 (0)
+	#define ProfThreadName(name_cstr) (0)
+#endif
+
+#endif // BASE_PROFILE_H
