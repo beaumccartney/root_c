acce4e2656d9ea8f656f41a9466fb4e07b34b5fe
Beau McCartney <mccartney.beausl@gmail.com>
2025-01-22 19:26:57 -0700

    port windows build to python

---
+++ build-windows.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+import sys
+import subprocess
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+os.chdir(script_dir)
+
+options = set(sys.argv[1:])
+
+exit_code = 0
+
+command = [
+    "cl",
+    "/I..\\layers\\",
+    "/I..\\local\\",
+    "/I..\\thirdparty\\",
+    "/nologo",
+    "/Z7",
+    "/FC",
+    "-Gm-",
+    "/W4",
+    "/wd4100",
+    "/wd4201",
+]
+
+link_flags = [
+    "/link",
+    "/MANIFEST:EMBED",
+    "/INCREMENTAL:NO",
+    "/noexp",
+]
+
+# used to check membership in the set, non pruned elements are unused (for warning)
+def check_and_remove(options, option):
+    result = option in options
+    options.discard(option)
+    return result
+
+if check_and_remove(options, "release"):
+    command += ("-O2", "-DBUILD_DEBUG=0")
+    print("[release mode]")
+else:
+    options.add("debug")
+
+if check_and_remove(options, "debug"):
+    command += ("/Od", "/Ob1", "/DBUILD_DEBUG=1")
+    link_flags.append("/DEBUG:FULL")
+    print("[debug mode]")
+
+if check_and_remove(options, "profile"):
+    options.add("tracy")
+
+if check_and_remove(options, "tracy"):
+    if "superluminal" in options:
+        print("[ERROR] tracy and superluminal profiling enabled - please choose one. Exiting...")
+        exit(1)
+    command += ("-DPROFILE_TRACY", "/MD")
+    print("[tracy profiling enabled]")
+
+if check_and_remove(options, "superluminal"):
+    command += ("-DPROFILE_SUPERLUMINAL", "/MD")
+    print("[superluminal profiling enabled]")
+
+
+if check_and_remove(options, "sanitize"):
+    options |= {"asan", "RTC"}
+
+if check_and_remove(options, "asan"):
+    if "RTC" in options:
+        print("[ERROR] RTC and address sanitizer enabled but they're incompatible - please choose one. Exiting...")
+        exit(1)
+    command.append("/fsanitize=address")
+    print("[asan enabled]")
+
+if check_and_remove(options, "RTC") or check_and_remove(options, "rtc"):
+    command.append("/RTCsu")
+    print("[RTC enabled]")
+
+if check_and_remove(options, "analyze"):
+    # REVIEW(beau): should I be doing this?
+    # flag = "--analyze:only" if "check" in options else "--analyze"
+    command.append("--analyze")
+    print("[static analyzer]")
+
+if check_and_remove(options, "check") or check_and_remove(options, "c"):
+    command.append("/Zs")
+    print("[check only]")
+
+os.makedirs("local", exist_ok=True)
+os.makedirs("build", exist_ok=True)
+os.chdir("build")
+
+# pdbs accumulate stale debug info
+for file in glob.glob("*.pdb"):
+    os.remove(file)
+
+build_targets = options & {
+    "leetcode",
+    "scratch",
+    "opengl",
+}
+
+for target in build_targets:
+    print(f"[build {target}]")
+    options.remove(target)
+    file_and_out = [f"../local/{target}.c", f"/Fe{target}"]
+    this_command = command + file_and_out + link_flags
+    if "verbose" in options:
+        print(" ".join(this_command))
+    subprocess.run(this_command)
+
+options.discard("verbose")
+
+if not build_targets:
+    print("[WARNING] no valid build target specified")
+    exit_code = 1
+
+if options:
+    unused_string = ", ".join([f"'{option}'" for option in options])
+    print("[WARNING] unused option(s):", unused_string)
+    exit_code = 1
+
+exit(exit_code)
--- build.bat
+++ build.bat
@@ -1,32 +1,7 @@
+:: wraps the python script and forwards all arguments to save typing python3 ...
 @echo off
-setlocal enabledelayedexpansion
-cd /D "%~dp0"
+pushd "%~dp0"
 
-for %%a in (%*) do set "%%a=1"
-if not "%release%"=="1" set debug=1
-if "%debug%"=="1"       set release=0 && echo [debug mode]
-if "%release%"=="1"     set debug=0 && echo [release mode]
-
-:: common cl flags
-set flags=/I..\layers\ /I..\local\ /I..\thirdparty\ /nologo /Z7 /FC -Gm- /W4 /wd4100 /wd4201
-
-if "%debug%"=="1"        set flags=%flags% /Od /Ob1 /DBUILD_DEBUG=1
-if "%release%"=="1"      set flags=%flags% /O2 /DBUILD_DEBUG=0
-if "%profile%"=="1"      set superluminal=1
-if "%superluminal%"=="1" set flags=%flags% -DPROFILE_SUPERLUMINAL=1 && (echo [superluminal profiling enabled])
-if "%tracy%"=="1"        set flags=%flags% -DPROFILE_TRACY=1 /MD    && (echo [tracy profiling enabled])
-if "%asan%"=="1"         set flags=%flags% -fsanitize=address       && (echo [asan enabled])
-
-:: RTCu will trip on asan itself if its on, so only turn it on if no asan
-if "%RTC%"=="1"   (if "%asan%"=="1" set flags=%flags% /RTCsu && (echo [RTC enabled]) else (set flags=%flags% /RTCcsu))
-
-if not exist build mkdir build
-if not exist local mkdir local
-
-pushd build
-
-if exist *.pdb del *.pdb
-
-cl %flags% /Fe:local ..\local\main.c /link /DEBUG:FULL /MANIFEST:EMBED /INCREMENTAL:NO
+python3 build-windows.py %*
 
 popd
