d43b2c268695a3ea354da400037fa403da71a6ce
Beau McCartney <mccartney.beausl@gmail.com>
2025-02-01 13:57:17 -0700

    all max integer constants are const

--- layers/base/base_core.h
+++ layers/base/base_core.h
@@ -324,20 +324,20 @@ internal Compiler compiler_from_context(void);
 
 #define Unused(name) (void)(name)
 
-global U64 max_U64 = UINT64_MAX;
-global U32 max_U32 = UINT32_MAX;
-global U16 max_U16 = UINT16_MAX;
-global U8  max_U8  = UINT8_MAX;
+const global U64 max_U64 = UINT64_MAX;
+const global U32 max_U32 = UINT32_MAX;
+const global U16 max_U16 = UINT16_MAX;
+const global U8  max_U8  = UINT8_MAX;
 
-global S64 max_S64 = INT64_MAX;
-global S32 max_S32 = INT32_MAX;
-global S16 max_S16 = INT16_MAX;
-global S8  max_S8  = INT8_MAX;
+const global S64 max_S64 = INT64_MAX;
+const global S32 max_S32 = INT32_MAX;
+const global S16 max_S16 = INT16_MAX;
+const global S8  max_S8  = INT8_MAX;
 
-global S64 min_S64 = INT64_MIN;
-global S32 min_S32 = INT32_MIN;
-global S16 min_S16 = INT16_MIN;
-global S8  min_S8  = INT8_MIN;
+const global S64 min_S64 = INT64_MIN;
+const global S32 min_S32 = INT32_MIN;
+const global S16 min_S16 = INT16_MIN;
+const global S8  min_S8  = INT8_MIN;
 
 #define bitmask1  0x00000001u
 #define bitmask2  0x00000003u
