9b78e9c63622c5f712f5bbc7e30584a997407791
Beau McCartney <mccartney.beausl@gmail.com>
2025-03-24 20:03:47 -0600

    init receipts in base entry point are marked unused by default
    layers that depend on a given layer may not be used, entirely validly

--- layers/base/base_entry_point.c
+++ layers/base/base_entry_point.c
@@ -6,12 +6,11 @@ internal void main_thread_base_entry_point(String8Array cmdline_args)
 	g_cmdline = cmd_line_from_args(scratch.arena, cmdline_args);
 
 	#ifdef OS_GFX_H
-	OS_GFX_InitReceipt gfx_receipt = os_gfx_init();
+	OS_GFX_InitReceipt gfx_receipt = os_gfx_init(); Unused(gfx_receipt);
 	#endif
 
 	#ifdef RENDER_H
-	R_InitReceipt r_receipt = r_init(gfx_receipt);
-	Unused(r_receipt);
+	R_InitReceipt r_receipt = r_init(gfx_receipt); Unused(r_receipt);
 	#endif
 
 	entry_point();
